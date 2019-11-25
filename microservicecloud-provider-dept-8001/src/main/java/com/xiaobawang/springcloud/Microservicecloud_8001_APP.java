fb4101298c0ca2108293d9450a16d9929aefccd3
DT <1466053692@qq.com>
2019-11-25 21:20:02 +0800

    Initial commit

---
+++ microservicecloud-provider-dept-8001/src/main/java/com/xiaobawang/springcloud/Microservicecloud_8001_APP.java
@@ -0,0 +1,13 @@
+package com.xiaobawang.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Microservicecloud_8001_APP {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Microservicecloud_8001_APP.class,args);
+    }
+
+}
