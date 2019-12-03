e57e2194664aeeb520c2bb108a43934bb2191d81
DT <1466053692@qq.com>
2019-12-03 22:12:01 +0800

    dashbord and zuul(01)

---
+++ microservicecloud-consumer-hystrix-dashboard/src/main/java/com/xiaobawang/springcloud/DeptConsumer_DashBoard_App.java
@@ -0,0 +1,13 @@
+package com.xiaobawang.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@EnableHystrixDashboard
+@SpringBootApplication
+public class DeptConsumer_DashBoard_App {
+    public static void main(String[] args) {
+        SpringApplication.run(DeptConsumer_DashBoard_App.class,args);
+    }
+}
