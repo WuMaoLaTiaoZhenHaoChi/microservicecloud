da218981f6f87d1b45e54ee9f25f974c1661489f
DT <1466053692@qq.com>
2021-06-21 16:28:43 +0800

    dashbord and zuul(01)

--- microservicecloud-eureka-7001/src/main/java/com/xiaobawang/springcloud/EurekaServer7001_App.java
+++ microservicecloud-eureka-7001/src/main/java/com/xiaobawang/springcloud/EurekaServer7001_App.java
@@ -3,13 +3,12 @@ package com.xiaobawang.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaServer7001_App {
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServer7001_App.class,args);
-
-        
+        ConfigurableApplicationContext run = SpringApplication.run(EurekaServer7001_App.class, args);
     }
 }
